c3b4c18d3b8dcb2a43e4a27947f7a43e7f2066c1
Rakshith2597 <rakshith.sathish@gmail.com>
2019-04-17 21:21:45 +0530

    completed login, need to do session

--- auth.py
+++ auth.py
@@ -10,6 +10,23 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    #remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if user actually exists
+    # take the user supplied password, hash it, and compare it to the hashed password in database
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    return redirect(url_for('main.index'))
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
